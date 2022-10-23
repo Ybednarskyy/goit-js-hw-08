ca71e8a85f97886012e3e0eae9aecebd6c2b0cd3
Ybednarskyy <y.bednarskyy@gmail.com>
2022-10-23 19:56:59 +0300

    Add basic markup

---
+++ src/js/01-gallery.js
@@ -0,0 +1,9 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import { showMesage } from './02-video';
+showMesage('Hello world');
+console.log(showMesage);
---
+++ src/js/02-video.js
@@ -0,0 +1,3 @@
+export const showMesage = mesage => {
+  console.log(mesage);
+};
